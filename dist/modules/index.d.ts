f19400e5832d88b48d076b99bbea3e4cfd6803f4
BenoitSerrano <benoit.serrano10@gmail.com>
2022-01-26 10:59:07 +0100

    add indexes

---
+++ dist/modules/index.d.ts
@@ -0,0 +1,3 @@
+import { buildAuthenticator, passwordTimeValidityStatus, passwordTimeValidityStatusType } from './authenticator';
+export { buildAuthenticator, passwordTimeValidityStatus };
+export type { passwordTimeValidityStatusType };
---
+++ dist/modules/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.passwordTimeValidityStatus = exports.buildAuthenticator = void 0;
+var authenticator_1 = require("./authenticator");
+Object.defineProperty(exports, "buildAuthenticator", { enumerable: true, get: function () { return authenticator_1.buildAuthenticator; } });
+Object.defineProperty(exports, "passwordTimeValidityStatus", { enumerable: true, get: function () { return authenticator_1.passwordTimeValidityStatus; } });
